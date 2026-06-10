8c0318506dcc9c6af4e6a10b7497c06170e5cf62
Dušan Tešić <dusan024@gmail.com>
2026-06-10 20:06:05 +0200

    Implemented DataStore and CryptoUtil

--- src/main/java/org/unibl/etf/krz/evoting/crypto/CryptoUtil.java
+++ src/main/java/org/unibl/etf/krz/evoting/crypto/CryptoUtil.java
@@ -1,4 +1,247 @@
 package org.unibl.etf.krz.evoting.crypto;
 
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
+
 public class CryptoUtil {
+
+    private static final String RSA_ALGORITHM = "RSA";
+    private static final int RSA_KEY_SIZE = 2048;
+
+    private static final String AES_ALGORITHM = "AES";
+    private static final String AES_TRANSFORMATION = "AES/CBC/PKCS5Padding";
+    private static final int AES_KEY_SIZE = 256;
+    private static final int AES_IV_SIZE = 16;
+
+    private static final String RSA_WRAP_TRANSFORMATION = "RSA/ECB/OAEPWithSHA-256andMGF1Padding";
+
+    private static final String SIGNATURE_ALGORITHM = "SHA256withRSA";
+    private static final String HMAC_ALGORITHM = "HmacSHA256";
+
+    private static final String HASH_ALGORITHM = "SHA-256";
+    private static final int SALT_SIZE = 16;
+
+    public static String generateSalt() {
+        byte[] salt = new byte[SALT_SIZE];
+        new SecureRandom().nextBytes(salt);
+        return bytesToHex(salt);
+    }
+
+    public static String hashPassword(String password, String saltHex) throws NoSuchAlgorithmException {
+        byte[] salt = hexToBytes(saltHex);
+        MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+        md.update(salt);
+        byte[] hash = md.digest(password.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hash);
+    }
+
+    public static boolean verifyPassword(String password, String saltHex, String expectedHashHex) throws NoSuchAlgorithmException {
+        String actualHash = hashPassword(password, saltHex);
+        return actualHash.equals(expectedHashHex);
+    }
+
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException, NoSuchProviderException {
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance(RSA_ALGORITHM, "BC");
+        kpg.initialize(RSA_KEY_SIZE, new SecureRandom());
+        return kpg.generateKeyPair();
+    }
+
+    public static PKCS10CertificationRequest buildCSR(String subjectDN, KeyPair keyPair) throws Exception {
+        ContentSigner signer = new JcaContentSignerBuilder(SIGNATURE_ALGORITHM)
+                .setProvider("BC")
+                .build(keyPair.getPrivate());
+        return new JcaPKCS10CertificationRequestBuilder(new X500Name(subjectDN), keyPair.getPublic())
+                .build(signer);
+    }
+
+    public static void saveCertificate(X509Certificate certificate, String path) throws CertificateEncodingException, IOException {
+        try (FileOutputStream os = new FileOutputStream(path)) {
+            os.write(certificate.getEncoded());
+        }
+    }
+
+    public static X509Certificate loadCertificate(String path) throws CertificateException, IOException, NoSuchProviderException {
+        try (FileInputStream is = new FileInputStream(path)) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
+            return (X509Certificate) cf.generateCertificate(is);
+        }
+    }
+
+    public static String certificateToBase64(X509Certificate certificate) throws CertificateEncodingException {
+        return Base64.getEncoder().encodeToString(certificate.getEncoded());
+    }
+
+    public static X509Certificate certificateFromBase64(String base64) throws CertificateException,IOException, NoSuchProviderException {
+        byte[] bytes = Base64.getDecoder().decode(base64);
+        try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes)) {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509", "BC");
+            return (X509Certificate) cf.generateCertificate(bis);
+        }
+    }
+
+    public static void saveToKeystore(PrivateKey privateKey, X509Certificate certificate, String alias, String password, String path) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("PKCS12", "BC");
+        keyStore.load(null, null);
+        keyStore.setKeyEntry(
+                alias,
+                privateKey,
+                password.toCharArray(),
+                new Certificate[]{ certificate }
+        );
+        try (FileOutputStream os = new FileOutputStream(path)) {
+            keyStore.store(os, password.toCharArray());
+        }
+    }
+
+    public static PrivateKey loadPrivateKeyFromKeystore(String alias, String password, String path) throws Exception {
+        KeyStore keyStore = KeyStore.getInstance("PKCS12", "BC");
+        try (FileInputStream is = new FileInputStream(path)) {
+            keyStore.load(is, password.toCharArray());
+        }
+        PrivateKey key = (PrivateKey) keyStore.getKey(alias, password.toCharArray());
+        if (key == null) {
+            throw new KeyStoreException("Private key not found for alias: " + alias);
+        }
+        return key;
+    }
+
+    public static SecretKey generateAESKey() throws NoSuchAlgorithmException, NoSuchProviderException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES_ALGORITHM, "BC");
+        keyGenerator.init(AES_KEY_SIZE, new SecureRandom());
+        return keyGenerator.generateKey();
+    }
+
+    public static EncryptionResult encryptAES(byte[] plaintext, SecretKey key) throws Exception {
+        byte[] ivBytes = new byte[AES_IV_SIZE];
+        new SecureRandom().nextBytes(ivBytes);
+        IvParameterSpec iv = new IvParameterSpec(ivBytes);
+
+        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION, "BC");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        byte[] ciphertext = cipher.doFinal(plaintext);
+
+        return new EncryptionResult(
+                Base64.getEncoder().encodeToString(ciphertext),
+                Base64.getEncoder().encodeToString(ivBytes)
+        );
+    }
+
+    public static byte[] decryptAES(String ciphertextBase64, String ivBase64, SecretKey key) throws Exception {
+        byte[] ciphertext = Base64.getDecoder().decode(ciphertextBase64);
+        byte[] ivBytes = Base64.getDecoder().decode(ivBase64);
+
+        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION, "BC");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ivBytes));
+        return cipher.doFinal(ciphertext);
+    }
+
+    public static String aesKeyToBase64(SecretKey key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static SecretKey aesKeyFromBase64(String base64) {
+        byte[] keyBytes = Base64.getDecoder().decode(base64);
+        return new SecretKeySpec(keyBytes, AES_ALGORITHM);
+    }
+
+    public static String encryptRSA(byte[] data, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(RSA_WRAP_TRANSFORMATION, "BC");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(data));
+    }
+
+    public static byte[] decryptRSA(String ciphertextBase64, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance(RSA_WRAP_TRANSFORMATION, "BC");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(Base64.getDecoder().decode(ciphertextBase64));
+    }
+
+    public static String sign(byte[] data, PrivateKey privateKey) throws Exception {
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM, "BC");
+        signature.initSign(privateKey);
+        signature.update(data);
+        return Base64.getEncoder().encodeToString(signature.sign());
+    }
+
+    public static boolean verifyBySignature(byte[] data, String signatureBase64, PublicKey publicKey) throws Exception {
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM, "BC");
+        signature.initVerify(publicKey);
+        signature.update(data);
+        return signature.verify(Base64.getDecoder().decode(signatureBase64));
+    }
+
+    public static SecretKey generateHMACKey() throws NoSuchAlgorithmException, NoSuchProviderException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(HMAC_ALGORITHM, "BC");
+        keyGenerator.init(256, new SecureRandom());
+        return keyGenerator.generateKey();
+    }
+
+    public static String computeHMAC(byte[] data, SecretKey key) throws Exception {
+        Mac mac = Mac.getInstance(HMAC_ALGORITHM, "BC");
+        mac.init(key);
+        return bytesToHex(mac.doFinal(data));
+    }
+
+    public static boolean verifyHMAC(byte[] data, SecretKey key, String expectedHex) throws Exception {
+        String actual = computeHMAC(data, key);
+        return MessageDigest.isEqual(hexToBytes(actual), hexToBytes(expectedHex));
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    private static byte[] hexToBytes(String hex) {
+        int len = hex.length();
+        byte[] data = new byte[len/2];
+        for (int i = 0; i < len; i += 2) {
+            data[i/2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4)
+                                + Character.digit(hex.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    public static class EncryptionResult {
+        private final String ciphertextBase64;
+        private final String ivBase64;
+
+        public EncryptionResult(String ciphertextBase64, String ivBase64) {
+            this.ciphertextBase64 = ciphertextBase64;
+            this.ivBase64 = ivBase64;
+        }
+
+        public String getCiphertextBase64() {
+            return ciphertextBase64;
+        }
+
+        public String getIvBase64() {
+            return ivBase64;
+        }
+    }
 }
--- src/main/java/org/unibl/etf/krz/evoting/storage/DataStore.java
+++ src/main/java/org/unibl/etf/krz/evoting/storage/DataStore.java
@@ -1,4 +1,235 @@
 package org.unibl.etf.krz.evoting.storage;
 
+import com.google.gson.*;
+import org.unibl.etf.krz.evoting.model.*;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DataStore {
+
+    public static final String ROOT = "data";
+    public static final String DIR_CA = ROOT + "/ca";
+    public static final String DIR_ORGANIZERS = ROOT + "/users/organizers";
+    public static final String DIR_VOTERS = ROOT + "/users/voters";
+    public static final String DIR_CERTIFICATES = ROOT + "/certificates";
+    public static final String DIR_KEYSTORES = ROOT + "/keystores";
+    public static final String DIR_POLLS = ROOT + "/polls";
+    public static final String DIR_VOTES = ROOT + "/votes";
+    public static final String DIR_HMAC = ROOT + "/hmac";
+    public static final String DIR_REPORTS = ROOT + "/reports";
+
+    public static final Gson GSON = new GsonBuilder()
+            .setPrettyPrinting()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .create();
+
+    public static void initialize() {
+        String[] dirs = {
+                DIR_CA, DIR_ORGANIZERS, DIR_VOTERS, DIR_CERTIFICATES,
+                DIR_KEYSTORES, DIR_POLLS, DIR_VOTES, DIR_HMAC, DIR_REPORTS
+        };
+        for (String dir : dirs) {
+            new File(dir).mkdirs();
+        }
+    }
+
+    public static void saveOrganizer(Organizer organizer) throws IOException {
+        String path = DIR_ORGANIZERS + "/" + organizer.getUsername() + ".json";
+        writeJson(path, organizer);
+    }
+
+    public static Organizer loadOrganizer(String username) throws IOException {
+        String path = DIR_ORGANIZERS + "/" + username + ".json";
+        return readJson(path, Organizer.class);
+    }
+
+    public static List<Organizer> loadAllOrganizers() throws IOException {
+        return loadAll(DIR_ORGANIZERS, Organizer.class);
+    }
+
+    public static boolean organizerExists(String username) {
+        return new File(DIR_ORGANIZERS + "/" + username + ".json").exists();
+    }
+
+    public static void saveVoter(Voter voter) throws IOException {
+        String path = DIR_VOTERS + "/" + voter.getUsername() + ".json";
+        writeJson(path, voter);
+    }
+
+    public static Voter loadVoter(String username) throws IOException {
+        String path = DIR_VOTERS + "/" + username + ".json";
+        return readJson(path, Voter.class);
+    }
+
+    public static List<Voter> loadAllVoters() throws IOException {
+        return loadAll(DIR_VOTERS, Voter.class);
+    }
+
+    public static boolean voterExists(String username) {
+        return new File(DIR_VOTERS + "/" + username + ".json").exists();
+    }
+
+    public static User loadUser(String username) throws IOException {
+        if (organizerExists(username)) {
+            return loadOrganizer(username);
+        }
+        if (voterExists(username)) {
+            loadVoter(username);
+        }
+        return null;
+    }
+
+    public static boolean userExists(String username) {
+        return organizerExists(username) || voterExists(username);
+    }
+
+    public static void savePoll(Poll poll) throws IOException {
+        String path = DIR_POLLS + "/" + poll.getPollId() + ".json";
+        writeJson(path, poll);
+    }
+
+    public static Poll loadPoll(String pollId) throws IOException {
+        String path = DIR_POLLS + "/" + pollId + ".json";
+        return readJson(path, Poll.class);
+    }
+
+    public static List<Poll> loadAllPolls() throws IOException {
+        return loadAll(DIR_POLLS, Poll.class);
+    }
+
+    public static List<Poll> loadOrganizerPolls(String organizerId) throws IOException {
+        return loadAllPolls().stream()
+                .filter(poll -> organizerId.equals(poll.getOrgId()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Poll> loadPollsStatus(Poll.Status status) throws IOException {
+        loadAllPolls().forEach(Poll::refreshStatus);
+        return loadAllPolls().stream()
+                .filter(poll -> status.equals(poll.getStatus()))
+                .collect(Collectors.toList());
+    }
+
+    public static void saveEncryptedVote(EncryptedVote vote) throws IOException {
+        String dir = DIR_VOTES + "/" + vote.getPollId();
+        new File(dir).mkdirs();
+        writeJson(dir + "/" + vote.geteVoteId() + ".json", vote);
+    }
+
+    public static List<EncryptedVote> loadPollVotes(String pollId) throws IOException {
+        String dir = DIR_VOTES + "/" + pollId;
+        return loadAll(dir, EncryptedVote.class);
+    }
+
+    public static boolean ifVoted(String pollId, String voterId) throws IOException {
+        List<EncryptedVote> votes = loadPollVotes(pollId);
+        for (EncryptedVote vote : votes) {
+            if (voterId.equals(vote.getVoterId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static EncryptedVote findVote(String voterId, String pollId) throws IOException {
+        List<EncryptedVote> votes = loadPollVotes(pollId);
+        for (EncryptedVote vote : votes) {
+            if (voterId.equals(vote.getVoterId())) {
+                return vote;
+            }
+        }
+        return null;
+    }
+
+    public static void saveHmac(String pollId, String hmacHex) throws IOException {
+        String path = DIR_HMAC + "/" + pollId + ".hmac";
+        Files.writeString(Paths.get(path), hmacHex);
+    }
+
+    public static String loadHmac(String pollId) throws IOException {
+        String path = DIR_HMAC + "/"  + pollId + ".hmac";
+        return Files.readString(Paths.get(path)).trim();
+    }
+
+    public static boolean hmacExists(String pollId) {
+        return new File(DIR_HMAC + "/" + pollId + ".hmac").exists();
+    }
+
+    public static void saveReport(PollReport report) throws IOException {
+        String path = DIR_REPORTS + "/" + report.getResult().getPollId() + ".json";
+        writeJson(path, report);
+    }
+
+    public static PollReport loadReport(String pollId) throws IOException {
+        String path = DIR_REPORTS + "/" + pollId + ".json";
+        return readJson(path, PollReport.class);
+    }
+
+    public static boolean reportExists(String pollId) {
+        return new File(DIR_REPORTS + "/" + pollId + ".json").exists();
+    }
+
+    public static String getCertificatePath(String username) {
+        return DIR_CERTIFICATES + "/" + username + ".cer";
+    }
+
+    public static String getKeystorePath(String username) {
+        return DIR_KEYSTORES + "/" + username + ".p12";
+    }
+
+    private static <T> void writeJson(String path, T object) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            GSON.toJson(object, writer);
+        }
+    }
+
+    private static <T> T readJson(String path, Class<T> clazz) throws IOException {
+        File file = new File(path);
+        if (!file.exists()) {
+            return null;
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            return GSON.fromJson(reader, clazz);
+        }
+    }
+
+    private static <T> List<T> loadAll(String dirPath, Class<T> clazz) throws IOException {
+        List<T> results = new ArrayList<>();
+        File dir = new File(dirPath);
+        if (!dir.exists() || !dir.isDirectory()) {
+            return results;
+        }
+        File[] files = dir.listFiles(f -> f.getName().endsWith(".json"));
+        if (files == null) return results;
+        for (File file : files) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                T obj = GSON.fromJson(reader, clazz);
+                if (obj != null) results.add(obj);
+            }
+        }
+        return results;
+    }
+
+    private static class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+
+        private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+        @Override
+        public JsonElement serialize(LocalDateTime src, Type type, JsonSerializationContext ctx) {
+            return new JsonPrimitive(src.format(FORMATTER));
+        }
+
+        @Override
+        public LocalDateTime deserialize(JsonElement json, Type type, JsonDeserializationContext ctx) throws JsonParseException {
+            return LocalDateTime.parse(json.getAsString(), FORMATTER);
+        }
+    }
 }
